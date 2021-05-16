b407e5bc02ea95e0afe2f92799736ce354ed5659
MuhammadJamshed <jaliawan93@gmail.com>
2021-05-16 20:02:43 +0500

    This is 2nd commit of kfc module front hand with object and hookers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 
 
 
+import { useState } from "react";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 
 function App() {
+  const [product, setProduct] = useState({
+    Name: "mouse",
+    price: 200,
+    quality: "excellent"
+  })
+  const update = () => {
+    setProduct({ ...product, price: product.price + 50 })
+  }
+  const degrate = () => {
+    setProduct({ ...product, price: product.price + 25 })
+  }
   return (
     <div className="container">
-      <Header />
-      <Home />
+      {/*  <Header />
+      <Home /> */}
+      <div>
+        <p>{product.Name}</p>
+        <p>{product.price}</p>
+        <p>{product.quality}</p>
+        <button onClick={update}>Increment</button>
+        <button onClick={degrate}>Decrement</button>
+      </div>
+
     </div>
   )
 };
